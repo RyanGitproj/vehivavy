75688ef0b94d6108fa3f5b7a9a42d5b3376c0cb8
JoyoRichard007 <joyop20.aps2b@gmail.com>
2024-11-26 10:12:14 +0300

    conf

--- conf.py
+++ conf.py
@@ -6,22 +6,26 @@ class Configuration:
         Retrieves the value from the environment.
         Takes the default value if not defined.
     '''
-    ADAPTER = env.get('ADAPTER')
+    ADAPTER = 'MYSQL'
 
-    DB_FILE = env.get('DB_FILE')
-    
-    DB_HOST = env.get('DB_HOST', 'localhost')
-    DB_USER = env.get('DB_USER', 'root')
-    DB_PASSWORD = env.get('DB_PASSWORD', '')
-    DB_PORT = env.get('DB_PORT')
-    DB_NAME = env.get('DB_NAME')
-    SRV_PROTOCOL = env.get('SRV_PROTOCOL')
+    # DB_FILE = 'ampalibe.db'
+    # DB_HOST = "34.28.32.137"
+    # DB_USER = 'root'
+    # DB_PASSWORD = '07ke6u%56O5x'
+    # DB_PORT = 3306
+    # DB_NAME = 'vosaryai'
+    # SRV_PROTOCOL = ''
 
-    ACCESS_TOKEN = env.get('AMP_ACCESS_TOKEN')
-    VERIF_TOKEN = env.get('AMP_VERIF_TOKEN')
+    DB_HOST = "localhost"
+    DB_USER = 'root'
+    DB_PASSWORD = ''
+    DB_PORT = 3306
+    DB_NAME = 'vehivavy'
+    SRV_PROTOCOL = ''
 
-    APP_HOST = env.get('AMP_HOST', '0.0.0.0')
-    APP_PORT = int(env.get('AMP_PORT', 4555))
-    APP_URL = env.get('AMP_URL')
-    
+    ACCESS_TOKEN = 'EAAWhes9F3lcBOwxtu73RG1JMIxtKgjBZCqsVsaQJrIZCFwRrOrytBEvAcCnCjNrJHYV2Voh2X5z9FqONNF8o5pkUhOYoNbQCNjLo0Qdg9MzPqFLqDZAqO0uy8viLyx4EtckudBzVRVxiOawOX1OQXNzNZCRfZCYQf1ekTlPact2UZC5e8ynv7AwmUuq4j6TLCiMAZDZD'
+    VERIF_TOKEN = 'VEHIVAVYAPP'
 
+    APP_HOST = "0.0.0.0"
+    APP_PORT = 4000
+    APP_URL = ''
